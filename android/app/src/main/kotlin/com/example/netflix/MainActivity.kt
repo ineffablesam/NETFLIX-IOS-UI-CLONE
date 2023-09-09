576a9137b29ab9a3eef551f4c9deb8c58d5c22fe
sam7075938131 <sam7075938131@gmail.com>
2023-09-10 01:58:12 +0530

    Fixed PIP Mode issue

---
+++ android/app/src/main/kotlin/com/example/netflix/MainActivity.kt
@@ -0,0 +1,23 @@
+import android.annotation.SuppressLint
+import android.content.res.Configuration
+import io.flutter.embedding.android.FlutterFragmentActivity
+import media.bcc.bccm_player.BccmPlayerPlugin
+
+class MainActivity : FlutterFragmentActivity() {
+    @SuppressLint("MissingSuperCall")
+    override fun onPictureInPictureModeChanged(
+        isInPictureInPictureMode: Boolean,
+        newConfig: Configuration
+    ) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        val bccmPlayer =
+            flutterEngine?.plugins?.get(BccmPlayerPlugin::class.javaObjectType) as BccmPlayerPlugin?
+        bccmPlayer?.handleOnPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+    }
+
+    override fun onBackPressed() {
+        if (!BccmPlayerPlugin.handleOnBackPressed(this)) {
+            super.onBackPressed()
+        }
+    }
+}
